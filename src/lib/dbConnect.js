6bdbca09d1c3fe0fae1614fe88c802e537e7fb9f
prince-bhalala <princebhalala24@gmail.com>
2025-07-19 13:47:29 +0530

    fix: comment out MONGODB_URI error check for flexibility in environment setup

--- src/lib/dbConnect.js
+++ src/lib/dbConnect.js
@@ -2,9 +2,9 @@ import mongoose from "mongoose";
 
 const MONGODB_URI = process.env.MONGODB_URI;
 
-if (!MONGODB_URI) {
-  throw new Error("❌ Please define MONGODB_URI in .env");
-}
+// if (!MONGODB_URI) {
+//   throw new Error("❌ Please define MONGODB_URI in .env");
+// }
 
 let cached = global.mongoose || { conn: null, promise: null };
 
